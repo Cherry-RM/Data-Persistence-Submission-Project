e8eb90613fe473556dbeaa84d62dcbcac887b273
Cherry-RM <94450819+Cherry-RM@users.noreply.github.com>
2021-11-17 20:52:55 +1100

    Finished Project
    Functionality completed

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
@@ -22,12 +21,20 @@ public class MainManager : MonoBehaviour
     [SerializeField] Text bestScoreText;
     private string playerName;
 
+    private void Awake()
+    {
+        //As soon as Game Starts it retrieves the High Score saved in Json
+        SavedData.Instance.GetHighScore();
+    }
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        
         playerName = SavedData.Instance.playerName;
-        bestScoreText.text = "Placeholder for now: " + playerName;
+
+        string bestPlayerNameAndScore = SavedData.Instance.bestPlayerNameAndScore;
+        bestScoreText.text = bestPlayerNameAndScore;
 
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
@@ -65,9 +72,14 @@ public class MainManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
             }
         }
     }
+
+
+
+
 
     void AddPoint(int point)
     {
@@ -77,81 +89,13 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        if (m_Points > SavedData.Instance.bestHighScore)
+        {
+            bestScoreText.text = "High Score: " + playerName + " : " + m_Points;
+            SavedData.Instance.SaveHighScore("High Score: " + playerName + " : " + m_Points, m_Points);
+        }
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
 
-
-
-
-
-    /*
-
-public Brick BrickPrefab;
-public int LineCount = 6;
-public Rigidbody Ball;
-
-public Text ScoreText;
-public GameObject GameOverText;
-
-private bool m_Started = false;
-private int m_Points;
-
-private bool m_GameOver = false;
-
-
-// Start is called before the first frame update
-void Start()
-{
-    const float step = 0.6f;
-    int perLine = Mathf.FloorToInt(4.0f / step);
-
-    int[] pointCountArray = new [] {1,1,2,2,5,5};
-    for (int i = 0; i < LineCount; ++i)
-    {
-        for (int x = 0; x < perLine; ++x)
-        {
-            Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-            var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-            brick.PointValue = pointCountArray[i];
-            brick.onDestroyed.AddListener(AddPoint);
-        }
-    }
-}
-
-private void Update()
-{
-    if (!m_Started)
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            m_Started = true;
-            float randomDirection = Random.Range(-1.0f, 1.0f);
-            Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-            forceDir.Normalize();
-
-            Ball.transform.SetParent(null);
-            Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-        }
-    }
-    else if (m_GameOver)
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-    }
-}
-
-void AddPoint(int point)
-{
-    m_Points += point;
-    ScoreText.text = $"Score : {m_Points}";
-}
-
-public void GameOver()
-{
-    m_GameOver = true;
-    GameOverText.SetActive(true);
-}*/
 }
--- Assets/Scripts/SavedData.cs
+++ Assets/Scripts/SavedData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SavedData : MonoBehaviour
 {
@@ -8,8 +9,16 @@ public class SavedData : MonoBehaviour
 
     public string playerName;
 
-    public int bestScore;
+    public string bestPlayerNameAndScore; //To be saved in Json
+    public int bestHighScore;
 
+    [System.Serializable]
+    class SaveData
+    {
+        public string bestPlayerNameAndScore;
+        public int bestHighScore;
+    }
+    
     private void Awake()
     {
         if (Instance != null)
@@ -20,9 +29,41 @@ public class SavedData : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(this.gameObject);
+
+        //For resetting High Score during Test
+        //ResetHighScore();
     }
 
     //Method to save json of best player name and score
+    public void SaveHighScore(string newStats, int newHighScore)
+    {
+        SaveData data = new SaveData();
+        data.bestPlayerNameAndScore = newStats;
+        data.bestHighScore = newHighScore;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
 
     //Method to retrieve json best player name and score
+    public void GetHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            bestPlayerNameAndScore = data.bestPlayerNameAndScore;
+            bestHighScore = data.bestHighScore;
+        }
+    }
+
+    //Resetting High Score for Testing
+    public void ResetHighScore()
+    {
+        SaveData data = new SaveData();
+        data.bestPlayerNameAndScore = "";
+        data.bestHighScore = 000;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
 }
